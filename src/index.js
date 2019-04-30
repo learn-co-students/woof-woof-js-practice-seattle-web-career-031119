e546990e7af277b5f6056c7d5d7163a91d129ce3
jyang81 <joe.yang.design@gmail.com>
2019-04-29 19:55:14 -0700

    Done. including bonus!

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const DOG_URL = "http://localhost:3000/pups"
-let CURRENT_DOG = null
-
+// let CURRENT_DOG = null
+let DOGS = null
 
 document.addEventListener('DOMContentLoaded', () => {
 
@@ -11,6 +11,7 @@ function getDogs(){
 }
 
 function showAllDogs(dogs){
+  DOGS = dogs
   dogs.forEach(dog => {
     // console.log('dog:', dog.name)
     makeDog(dog)
@@ -23,46 +24,45 @@ function makeDog(dog){
   span.textContent = dog.name
   bar.appendChild(span)
   span.addEventListener('click', () => {
-    showDogInfo(dog)
+    clearDog()
+  // CURRENT_DOG = dog
+  let box = document.getElementById('dog-info')
+  let img = document.createElement('img')
+  img.src = dog.image
+  let h2 = document.createElement('h2')
+  h2.textContent = dog.name
+  let button = document.createElement('button')
+  button.id = 'good-btn'
+  box.appendChild(img)
+  box.appendChild(h2)
+  box.appendChild(button)
+  button.addEventListener('click', () => {
+    changeStatus(dog)
   })
+  isDogGood(dog)
+
+})
 }
 
-// What to put in dog box
-// <img src=dog_image_url>
-// <h2>Mr. Bonkers</h2>
-// <button>Good Dog!</button>
-function showDogInfo(dog){
-  img.src = dog.image
-  h2.textContent = dog.name
-  button.id = dog.id
+function clearDog(){
+  let dogInfo = document.getElementById('dog-info')
+  while (dogInfo.firstChild){
+    dogInfo.firstChild.remove()
+  }
+}
+
+function isDogGood(dog){
+  let button = document.getElementById('good-btn')
   if (dog.isGoodDog === true){
     button.textContent = 'Good dog!'
   }
   else {
     button.textContent = 'Bad dog!'
   }
-  button.addEventListener('click', () => {
-    changeStatus(dog)
-    // console.log(dog)
-  })
-}
-// --status--
-// isGoodDog: true
-
-
-function makeDogContainer(){
-  box = document.getElementById('dog-info')
-  img = document.createElement('img')
-  h2 = document.createElement('h2')
-  button = document.createElement('button')
-
-  box.appendChild(img)
-  box.appendChild(h2)
-  box.appendChild(button)
 }
 
 function changeStatus(dog){
-  button = document.getElementById(dog.id)
+  let button = document.getElementById('good-btn')
   if (dog.isGoodDog === true) {
     dog.isGoodDog = false;
     button.textContent = 'Bad dog!'
@@ -87,11 +87,42 @@ function updateStatus(dog) {
     })
   })
   .then(res => res.json())
-
 }
 
 
+
+function filterDogs(){
+  let button = document.getElementById('good-dog-filter')
+  button.addEventListener('click', () => {
+    if (button.innerText === 'Filter good dogs: OFF') {
+      button.innerText = 'Filter good dogs: ON';
+      showGoodDogs()
+    }
+    else if (button.innerText === 'Filter good dogs: ON') {
+      button.innerText = 'Filter good dogs: OFF';
+      clearDogs()
+      showAllDogs(DOGS)
+    }
+  })
+}
+
+function showGoodDogs(){
+  clearDogs()
+  DOGS.forEach(dog => {
+  if (dog.isGoodDog === true){
+    makeDog(dog)
+  }
+  })
+}
+
+function clearDogs(){
+  let dogBar = document.getElementById('dog-bar')
+  while (dogBar.firstChild){
+    dogBar.firstChild.remove()
+  }
+}
+
 getDogs()
-makeDogContainer()
+filterDogs()
 
 })
